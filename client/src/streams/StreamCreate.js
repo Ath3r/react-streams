1562628de5336ec337d82530b96c09393e4e433f
Ather <mailtoather17@gmail.com>
2021-02-15 18:36:23 +0530

    server setup

--- client/src/streams/StreamCreate.js
+++ client/src/streams/StreamCreate.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
-
+import { connect } from 'react-redux';
+import { createStream } from '../actions';
 class StreamCreate extends Component {
 	renderInput = ({ input, label, meta }) => {
 		const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
@@ -11,9 +12,9 @@ class StreamCreate extends Component {
 			</div>
 		);
 	};
-	onSubmit(formValues) {
-		console.log(formValues);
-	}
+	onSubmit = (formValues) => {
+		this.props.createStream(formValues);
+	};
 	render() {
 		return (
 			<form
@@ -40,7 +41,11 @@ const validate = (formValues) => {
 	}
 	return errors;
 };
-export default reduxForm({
+const formWrapped = reduxForm({
 	form: 'streamCreate',
 	validate: validate,
 })(StreamCreate);
+
+export default connect(null, {
+	createStream,
+})(formWrapped);
